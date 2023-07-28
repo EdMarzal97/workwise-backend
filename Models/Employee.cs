c61e9b3686f5126f05d5f81229514b9b2de30439
Edward S Marzal <edsanchezmarzal@gmail.com>
2023-07-27 22:01:01 -0500

    api done CRUD

---
+++ Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkWiseApi.Models
+{
+    public class Employee
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? email { get; set; }
+        public string? team { get; set; }
+    }
+}
